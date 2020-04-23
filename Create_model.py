614418e96d1d4b90b6736624be67bd7364bc1398
Prasanna-icefire <prasannamg3@gmail.com>
2020-04-23 21:06:23 +0530

    Model Saved and ready to be deployed

--- Create_model.py
+++ Create_model.py
@@ -16,13 +16,11 @@ y = pickle.load(open("y.pickle","rb"))
 X=X/255.0
 print(X[1])
 model = Sequential()
-
 model.add(Conv2D(64,(3,3),input_shape=X.shape[1:]))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(64,(3,3)))
 model.add(Activation("relu"))
-
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(64,(3,3)))
 model.add(Activation("relu"))
@@ -34,4 +32,6 @@ model.add(Dense(1))
 model.add(Activation('sigmoid'))
 model.compile(loss="binary_crossentropy",optimizer="adam",metrics=['accuracy'])
 model.fit(np.array(X),np.array(y), batch_size=32,epochs = 6, validation_split=0.1,callbacks=[tensorboard])
+
+model.save('Dog_CAT_CNN.model')
 
