bc501c9971348e43578a5371dd1e04363741208c
Prasanna-icefire <prasannamg3@gmail.com>
2020-04-26 23:03:38 +0530

    Added a single variable for user to test code

--- predictions.py
+++ predictions.py
@@ -2,12 +2,17 @@ import tensorflow as tf
 import cv2
 CATEGORIES = ["Dog","Cat"]
 IMAGE_SIZE=60
+
+
+pic = "dog.jpg"
+
+
 def prepare(filepath):
     IMAGE_SIZE = 60
     img_array = cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
     new_array = cv2.resize(img_array,(IMAGE_SIZE,IMAGE_SIZE))
     return new_array.reshape(-1,IMAGE_SIZE,IMAGE_SIZE,1)
 model = tf.keras.models.load_model("Dog_CAT_CNN.model")
-prediction = model.predict([prepare("dog.jpg")])
+prediction = model.predict([prepare(pic)])
 print(CATEGORIES[int(prediction[0][0])])
 
